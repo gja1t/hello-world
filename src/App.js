b155951807c08ed47c56784621af6b1a4ff08b46
Gjalt Reitsma <gjalt.reitsmaonline.nl>
2025-01-19 20:53:46 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello World!</h1>
+      <p>Welcome to my first React app hosted on GitHub Pages.</p>
+    </div>
+  );
+}
+
+export default App;
+
